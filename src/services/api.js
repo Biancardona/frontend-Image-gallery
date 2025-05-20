13decfdba449266bf5039b7724190778e958a1c2
biancardona <biancardona.rivera@gmail.com>
2025-05-19 19:30:44 -0600

    Fixing e variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = `${BASE_URL}${BASE_URL.endsWith('/api') ? '' : '/api'}`;
 
 const api = axios.create({
     baseURL: API_URL,
